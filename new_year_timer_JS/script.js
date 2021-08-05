68ed8835715f75dfd99eea9edf3fc60f6be7f1d7
d_pov <65812208+d-pov@users.noreply.github.com>
2021-08-05 15:07:52 +0300

    Add files via upload

---
+++ new_year_timer_JS/script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+const newYears= '1 Jan 2022';
+
+function countdown() {
+	const newYearsDate = new Date(newYears)
+	const currentDate = new Date()
+
+	const totalSeconds = (newYearsDate - currentDate) / 1000
+
+	const days = Math.floor(totalSeconds / 3600 / 24)
+	const hours	= Math.floor(totalSeconds / 3600) % 24
+	const mins = Math.floor(totalSeconds / 60) % 60
+	const seconds = Math.floor(totalSeconds) % 60
+
+	console.log(days, hours, mins, seconds)
+
+	daysEl.innerText = days
+	hoursEl.innerText = hours
+	minsEl.innerText = mins
+	secondsEl.innerText = seconds
+}
+
+countdown()
+setInterval(countdown,1000)
